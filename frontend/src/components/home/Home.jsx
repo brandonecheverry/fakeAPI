f89dd8dcd8a23e376f1989588e5184fa917ca93d
b.angulo@utp.edu.co <sansumgfor99>
2024-03-28 00:45:39 -0500

    se agrega funcionalidad al FrontEnd

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="fa fa-home" title="Home" subtitle="CRUD Proyecto React">
+        <div className="display-4">Bienvenido!</div>
+        <hr />
+        <p className="mb-0">CRUD realizado con REACT</p>
+    </Main>
---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Footer.css'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desarrollado por <strong>Brandon Echeverry - 2024</strong> <i className="fa fa-code text-danger"></i>
+        </span>
+    </footer>
